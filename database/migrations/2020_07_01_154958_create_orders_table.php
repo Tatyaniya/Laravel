417035ba835f53b662530f156ec11c70877e396c
Tatyaniya <tatyaniya@bigmir.net>
2020-07-04 19:47:42 +0300

    autoincrements

--- database/migrations/2020_07_01_154958_create_orders_table.php
+++ database/migrations/2020_07_01_154958_create_orders_table.php
@@ -14,7 +14,7 @@ class CreateOrdersTable extends Migration
     public function up()
     {
         Schema::create('orders', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->integer('product_id');
             $table->string('name');
             $table->string('email');
